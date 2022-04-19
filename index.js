7a53f3f74c78918bd4653245d2bc95636632c953
fchiave <fchiave@ad.unc.edu>
2022-04-19 15:56:43 -0400

    requirements

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
 const minimist = require('minimist')
 const args = minimist(process.argv.slice(2))
 
+// Require database.js
+const db = require('./database.js');
+// Require morgan
+const morgan = require("morgan")
+// Require fs
+const fs = require("fs")
+
 //if --help option is included, don't init anything, return help msg and exit
 if (args.help == true) {
     console.log(`server.js [options]
@@ -31,6 +38,8 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
 // start server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
@@ -38,3 +47,48 @@ const server = app.listen(port, () => {
 
 // Serve static HTML files
 app.use(express.static('./public'));
+
+// middleware to insert new record into database
+app.use( (req, res, next) => {
+    // Your middleware goes here.
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+    console.log(logdata)
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, secure, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.secure, logdata.status, logdata.referer, logdata.useragent)
+    next()
+    })
+
+    
+if (args.debug) {
+    app.get('/app/log/access', (req, res) => {
+        const stmt = db.prepare('SELECT * FROM accesslog').all()
+        res.status(200).json(stmt)
+    });
+
+    app.get('/app/error', (req, res) => {
+        throw new Error('Error test successful.') // Express will catch this on its own.
+    });
+
+    // add logging
+    if (args.log) { 
+        const WRITESTREAM = fs.createWriteStream('access.log', { flags: 'a'})
+        app.use(morgan('combined', {stream: WRITESTREAM}))   
+    }
+}
+
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
